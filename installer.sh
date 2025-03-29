3de788b384a8b3c9f48a86613d077ed4606d3634
mirroxin <160865695+mirroxin@users.noreply.github.com>
2025-03-29 12:04:45 +0300

    ыыы

--- installer.sh
+++ installer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Установка необходимых пакетов
-sudo pacman -S --needed git base-devel sudo pacman -S waybar hyprlock kitty rofi fastfetch
+sudo pacman -S --needed --noconfirm git base-devel sudo pacman -S waybar hyprlock kitty rofi fastfetch
 
 # Клонирование твоих дотов
 git clone https://github.com/mirroxin/rampage.git
 git clone https://aur.archlinux.org/yay.git ~/yay-installer
 cd ~/yay-installer
 makepkg -si --noconfirm
-yay -S swww pywal swaync wofi
+yay -S --noconfirm swww pywal swaync wofi ttf-monocraft noto-fonts-cjk otf-codenewroman-nerd
 
 # Переход в папку с конфигами
 cd ~/rampage
